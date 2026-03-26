f35acf9490fd21e6cc4b64a0b5a85c0fd99e3503
Amr Maharek <amrabdo4790@gmail.com>
2026-03-26 11:55:29 +0200

    update the main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "ast.h"
-#include "c12-lib.h"
+#include "cortex.h"
+#include <cstdlib>
 
 Result<u32> divide(u32 a, u32 b) {
     if (b == 0) {
@@ -9,22 +10,41 @@ Result<u32> divide(u32 a, u32 b) {
     return a / b;
 }
 
-int main(int argc, char** argv) {
-
-    if (argc < 2) {
+void check_args(int minimum, int argc, char** argv) {
+    if (argc < minimum) {
         fprintf(stderr, "usage: %s <FILE_NAME>\n", argv[0]);
         exit(1);
     }
+}
 
-    auto src_file = read_file(argv[1]); // create_file("unnamed", "1 + 2");
+int main(int argc, char* argv[]) {
+    (void)argc;
+    // check_args(2, argc, argv);
+    const char* example_1 = "test/syntax.c12";
+    const char* files[2] = {argv[1], example_1};
+    auto src_file = read_file(files[0]);
+
+    int inst[] = {1, 2, 3, 4, 5};
+    slice<int> sint = {inst, 5};
 
     Ast ast = new_ast(src_file);
     defer(ast.deinit());
 
-    println(stdout, "Parsing {}", ast.file_name);
-    auto res = parse_precedence(ast, 0);
+    auto res = parse_var_decl(ast); // declspec(ast)
+    if (ast.nodes[res].tag == AST_VAR_DECL) {
+        printf("var_name = %.*s\n", ast.token_of(res).buf);
+        auto init_index = ast.nodes[res].var.init_expr;
+        auto initexpr = ast.nodes[init_index];
+        printf("init expression = %s\n", to_str(initexpr.tag));
 
-    for (auto n : ast.nodes) {
-        println(stdout, "{}: {}", to_str(n.kind), ast.tokens[n.main_token].buf);
+        if (initexpr.tag == AST_CALL) {
+            for (auto e : initexpr.call.args) {
+                printf("arg = %s\n", to_str(ast.nodes[e].tag));
+            }
+        }
+    }
+
+    for (auto n : ast.types) {
+        printf("%s -> %s -> %lu\n", to_str(n.id), n.name.ptr, n.id);
     }
 }
