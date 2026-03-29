f3b5f6f08fa428c2b7641293db59f88e230db458
Amr Maharek <amrabdo4790@gmail.com>
2026-03-29 19:04:26 +0200

    use pp_node()

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,8 @@
 #include "ast.h"
-#include "cortex.h"
-#include <cstdlib>
 
+const char* pp_node(Node* n, String prefix = "");
 Result<u32> divide(u32 a, u32 b) {
-    if (b == 0) {
-        return Error("division by zero error");
-    }
+    if (b == 0) { return Error("division by zero error"); }
 
     return a / b;
 }
@@ -18,33 +15,18 @@ void check_args(int minimum, int argc, char** argv) {
 }
 
 int main(int argc, char* argv[]) {
+    defer(StringBuffer.deinit());
     (void)argc;
     // check_args(2, argc, argv);
     const char* example_1 = "test/syntax.c12";
     const char* files[2] = {argv[1], example_1};
     auto src_file = read_file(files[0]);
-
-    int inst[] = {1, 2, 3, 4, 5};
-    slice<int> sint = {inst, 5};
+    defer(src_file.deinit());
 
     Ast ast = new_ast(src_file);
     defer(ast.deinit());
 
-    auto res = parse_var_decl(ast); // declspec(ast)
-    if (ast.nodes[res].tag == AST_VAR_DECL) {
-        printf("var_name = %.*s\n", ast.token_of(res).buf);
-        auto init_index = ast.nodes[res].var.init_expr;
-        auto initexpr = ast.nodes[init_index];
-        printf("init expression = %s\n", to_str(initexpr.tag));
-
-        if (initexpr.tag == AST_CALL) {
-            for (auto e : initexpr.call.args) {
-                printf("arg = %s\n", to_str(ast.nodes[e].tag));
-            }
-        }
-    }
-
-    for (auto n : ast.types) {
-        printf("%s -> %s -> %lu\n", to_str(n.id), n.name.ptr, n.id);
-    }
+    auto res = parse_block(ast); // declspec(ast)
+    String prefix = ""; defer(prefix.destroy());
+	pp_node(res,prefix);
 }
